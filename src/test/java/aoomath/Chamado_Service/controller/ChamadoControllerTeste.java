c63466af11dd86fcbe18e51970fbcaa7df9fbe91
Matheus <aoomath25@gmail.com>
2025-12-01 20:10:07 -0300

    Configuração do Dockerfile e workflow para publicação no GitHub Actions

--- src/test/java/aoomath/Chamado_Service/controller/ChamadoControllerTeste.java
+++ src/test/java/aoomath/Chamado_Service/controller/ChamadoControllerTeste.java
@@ -22,7 +22,6 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
